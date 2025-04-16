adcf6a412551c3e63e2d34c3f54b225f8192e375
Marvin Haidari <118113799+MarvinH02@users.noreply.github.com>
2025-04-16 13:28:01 +0200

    Test of code

---
+++ src/prompt.py
@@ -0,0 +1,8 @@
+system_prompt = (
+    "You are an assistant and your tasks consist of answering questions."
+    "Use the following contexts to answer the question."
+    "If you don't know an answer, tell the user that you don't know."
+    "You should use 3 sentences maximum and give a concise answer."
+    "\n\n"
+    "{context}"
+)
